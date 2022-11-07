5a4a5f95842eb5a99455d57d47a7a7369d460155
Anastasia Rarova <07tangled07@gmail.com>
2022-11-07 23:15:29 +0300

    add and style the slider buttons for .homeslider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+// Testimonials Slider
 $(document).ready(function(){
     $('.testimonials_slider').slick({
       arrows: false,
       dots: true
+    });
+  });
+
+  // Top Slider
+
+  $(document).ready(function(){
+    $('.homeslider').slick({
+      prevArrow: `<button type="button" class="slick-prev"></button>`,
+      nextArrow: `<button type="button" class="slick-next"></button>`
     });
   });
