490f7bfb493484ee6e5cbda012966c4c22da5c53
Anastasia Rarova <07tangled07@gmail.com>
2022-11-21 10:13:31 +0300

    change the nav menu for max-width: 800px

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 // Testimonials Slider
-$(document).ready(function(){
-    $('.testimonials_slider').slick({
-      arrows: false,
-      dots: true
-    });
+$(document).ready(function () {
+  $('.testimonials_slider').slick({
+    arrows: false,
+    dots: true
   });
+});
 
-  // Top Slider
+// Top Slider
 
-  $(document).ready(function(){
-    $('.homeslider').slick({
-      prevArrow: `<button type="button" class="slick-prev"></button>`,
-      nextArrow: `<button type="button" class="slick-next"></button>`
-    });
+$(document).ready(function () {
+  $('.homeslider').slick({
+    prevArrow: `<button type="button" class="slick-prev"></button>`,
+    nextArrow: `<button type="button" class="slick-next"></button>`
   });
+});
+
+
+$(document).ready(function () {
+  $('.mobile_nav .open').on('click', function () {
+    if ($(this).text() == 'Open Menu') {
+      $('ul.mobile').show();
+
+      $(this).text('Close Menu');
+    } else {
+      $('ul.mobile').hide();
+      $(this).text('Open Menu');
+    }
+  });
+});
